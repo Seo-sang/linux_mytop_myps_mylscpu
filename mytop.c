33d6a112152f2083e438d71770a19a8cbb880402
Seo-sang <tjtkdgus45@nate.com>
2021-09-21 04:21:33 +0900

    add all options of top

--- mytop.c
+++ mytop.c
@@ -21,6 +21,7 @@
 #include <limits.h>
 
 #define MAX 1024
+
 struct tm *t;
 
 //헤더 정보들
@@ -38,10 +39,21 @@ int uptime_line; //uptime_line 표시 여부
 double delay; //refresh 초단위
 int option_c; //명령 인자 표시/ 비표시
 char string[MAX];
-int option_i;
+int option_i; //유휴 프로세스 표시하지 않는 옵션
+int print_cnt; //print 실행 횟수
+int print_max; //print 최대 실행 수
 
 int print_row; //result배열에 저장할 행 번
 
+int special_pid; //특정 pid만 출력하는 경우 pid 개수 
+int pids[MAX]; //출력 pid 넣는 함수
+
+int option_b; //나열모드 옵션
+
+int special_user; //특정 user만 출력하는 경우 user의 수
+char users[MAX]; //출력 user 넣는 함수
+
+int kill_pid; //signal을 보낼 pid
 //cpu읽은 정보
 int current_cpu[9];
 int before_cpu[9];
@@ -67,7 +79,8 @@ proc procs[MAX];
 
 char result[MAX][MAX];
 
-void print();
+void print_1();
+void print_2();
 void get_data();
 void sort_by_cpu();
 void sort_by_time();
@@ -85,6 +98,11 @@ long long get_value(const char* str) { //string으로부터 정수값을 얻는 
 }
 
 void handler(int signo) { //SIGALRM 핸들러 함수
+	if(print_max == print_cnt) {
+		endwin();
+		exit(0);
+	}
+	print_cnt++;
 	memset(result, 0, sizeof(result));
 	memset(procs, 0, sizeof(procs));
 	print_row = 0;
@@ -96,9 +114,13 @@ void handler(int signo) { //SIGALRM 핸들러 함수
 		sort_by_time();
 	else if(option == 'M')
 		sort_by_mem();
-
-	print();
-	refresh();
+	if(!option_b) {
+		print_1();
+		refresh();
+	}
+	else {
+		print_2();
+	}
 	alarm(delay);
 }
 
@@ -196,7 +218,7 @@ void get_cpu_info() { //proc/stat으로부터 cpu정보를 받는 함
 	hi = (double)(current_cpu[5] - before_cpu[5]) / (double)(current_cpu[8] - before_cpu[8]) *100;
 	si = (double)(current_cpu[6] - before_cpu[6]) / (double)(current_cpu[8] - before_cpu[8]) *100;
 	st = (double)(current_cpu[7] - before_cpu[7]) / (double)(current_cpu[8] - before_cpu[8]) *100;
-	
+
 	memcpy(before_cpu, current_cpu, sizeof(before_cpu));
 }
 
@@ -224,7 +246,7 @@ void get_mem_info() {//proc/meminfo로부터 정보를 얻는 함수
 		strcpy(mem[i++], ptr);
 		ptr = strtok(NULL, "\n"); //개행으로 정보 파싱
 	}
-	
+
 	//파싱한 정보 int형으로 바꾸기
 	int mtotal = get_value(mem[0]); 
 	int mfree = get_value(mem[1]); 
@@ -235,7 +257,7 @@ void get_mem_info() {//proc/meminfo로부터 정보를 얻는 함수
 	int stotal = get_value(mem[14]);
 	int sfree = get_value(mem[15]);
 	int mavail = get_value(mem[2]);
-	
+
 	//header 4행 정보 저장
 	memtotal = (double)mtotal / 1024;
 	memfree = (double)mfree / 1024;
@@ -245,7 +267,7 @@ void get_mem_info() {//proc/meminfo로부터 정보를 얻는 함수
 	swapfree = (double)sfree / 1024;
 	swapused = (double)(stotal - sfree) / 1024;
 	swapavail_mem = (double)mavail / 1024;
-	
+
 }
 
 void get_proc_stat(char* proc_stat_path, int index) { //proc/pid/stat에서 정보를 얻는 함
@@ -293,7 +315,9 @@ void get_proc_stat(char* proc_stat_path, int index) { //proc/pid/stat에서 정�
 	struct stat statbuf; //process stat구조체
 	stat(proc_stat_path, &statbuf); //해당 stat 읽기
 	struct passwd *upasswd = getpwuid(statbuf.st_uid); //uid읽어오기수
-	for(int i = 0; i < 7; i++) {
+	
+	//username 읽기
+	for(int i = 0; i < MAX; i++) {
 		if(upasswd->pw_name[i] != '\0') {
 			procs[index].USER[i] = upasswd->pw_name[i];
 		}
@@ -301,7 +325,6 @@ void get_proc_stat(char* proc_stat_path, int index) { //proc/pid/stat에서 정�
 			break;
 		}
 	}
-	if(upasswd->pw_name[7] != '\0') procs[index].USER[7] = '+';
 	strncpy(procs[index].PR, stats[17], 3); //PR저장
 	procs[index].NI = atoi(stats[18]); //NI저장
 
@@ -312,9 +335,9 @@ void get_proc_stat(char* proc_stat_path, int index) { //proc/pid/stat에서 정�
 
 	double tic = (double)(utime + stime) / hertz;
 	procs[index].CPU = (double)tic / uptime * 100;//%CPU구하기
-	procs[index].CPU = round(procs[index].CPU * 10) / 10; //소수점 첫째자리까지 반올림
+	procs[index].CPU = round(procs[index].CPU); //소수점 첫째자리까지 반올림
 	procs[index].TIME = (double)(utime + stime) / ((double)hertz / 100); //TIME+구하기
-	
+
 	//COMMAND저장
 	if(option_c) {
 		i = 1;
@@ -390,7 +413,7 @@ void get_procs() { //pid를 확인하고 process 정보들을 가져오는 함�
 			tasks++;
 		}
 	}
-	
+
 	char proc_stat_path[MAX]; //prod/pid/sta t경로
 	char proc_status_path[MAX]; //proc/pid/status 경로
 	char proc_cmdline_path[MAX]; //proc/pid/cmdline 경로
@@ -401,18 +424,20 @@ void get_procs() { //pid를 확인하고 process 정보들을 가져오는 함�
 		sprintf(proc_stat_path, "%s/%ld/stat", proc_path, procs[i].PID);
 		sprintf(proc_status_path, "%s/%ld/status", proc_path, procs[i].PID);
 		sprintf(proc_cmdline_path, "%s/%ld/cmdline", proc_path, procs[i].PID);
-		get_proc_stat(proc_stat_path, i); //proc/pid/stat의 정보를 얻는다.
-		get_proc_status(proc_status_path, i); //proc/pid/status의 정보를 얻는다.
-		if(option_c) get_cmdline(proc_cmdline_path, i);
+		if(access(proc_stat_path, F_OK) == 0) //파일이 존재할 때만 열기
+			get_proc_stat(proc_stat_path, i); //proc/pid/stat의 정보를 얻는다.
+		if(access(proc_status_path, F_OK) == 0) //파일이 존재할 때만 열기
+			get_proc_status(proc_status_path, i); //proc/pid/status의 정보를 얻는다.
+		if(option_c && access(proc_cmdline_path, F_OK) == 0) get_cmdline(proc_cmdline_path, i);
 	}
 	closedir(proc_dir);
 }
 
 
 void sort_by_cpu() { //%CPU순으로 정렬
-	for(int i = 0; i < tasks; i++) {
+	for(int i = 0; i < tasks-1; i++) {
 		int mnum = i;
-		for(int j = i+1; j <= tasks; j++) {
+		for(int j = i+1; j < tasks; j++) {
 			if(procs[mnum].CPU < procs[j].CPU) {
 				mnum = j;
 			}
@@ -425,9 +450,9 @@ void sort_by_cpu() { //%CPU순으로 정렬
 }
 
 void sort_by_time() { //TIME+순으로 정렬
-	for(int i = 0; i < tasks; i++) {
+	for(int i = 0; i < tasks-1; i++) {
 		int mnum = i;
-		for(int j = i+1; j <= tasks; j++) {
+		for(int j = i+1; j < tasks; j++) {
 			if(procs[mnum].TIME < procs[j].TIME) {
 				mnum = j;
 			}
@@ -440,9 +465,9 @@ void sort_by_time() { //TIME+순으로 정렬
 }
 
 void sort_by_mem() { //%MEM순으로 정렬
-	for(int i = 0; i < tasks; i++) {
+	for(int i = 0; i < tasks-1; i++) {
 		int mnum = i;
-		for(int j = i+1; j <= tasks; j++) {
+		for(int j = i+1; j < tasks; j++) {
 			if(procs[mnum].MEM < procs[j].MEM) {
 				mnum = j;
 			}
@@ -470,8 +495,8 @@ void get_data() {//데이터를 읽어오는 함수
 	get_mem_info();
 	get_cpu_info();
 	get_procs();
-	
-	memset(result, 0, sizeof(result));
+
+	memset(result, '\0', sizeof(result));
 	//head 저장
 	if(!uptime_line) {
 		sprintf(result[print_row++], "top - %02d:%02d:%02d up  %2d:%02d,%3d user,  load average: %s", t->tm_hour, t->tm_min, t->tm_sec, uptime_h, uptime_m, user, loadavg);
@@ -482,95 +507,333 @@ void get_data() {//데이터를 읽어오는 함수
 	sprintf(result[print_row++], "MiB Swap:   %.1f total,   %.1f free,   %.1f used.   %.1f avail Mem", swaptotal, swapfree, swapused, swapavail_mem);
 	strcpy(result[print_row++], "");
 	sprintf(result[print_row++], "%7s %-8s %3s %3s %7s %6s %6s %c %4s %4s   %7s %s",
-		"PID", "USER", "PR", "NI", "VIRT", "RES", "SHR", 'S', "%CPU", "%MEM", "TIME+", "COMMAND");
+			"PID", "USER", "PR", "NI", "VIRT", "RES", "SHR", 'S', "%CPU", "%MEM", "TIME+", "COMMAND");
 }
 
-void print() { //결과를 출력하는 함수
+bool ispid(int pid) { //출력해야하는 pid인지 판단
+	if(special_pid == 0) return true; //출력해야하는 pid의 수가 0인 경우 출력
+	bool ret = false;
+	for(int i = 0; i < special_pid; i++) {
+		if(pids[i] == pid) {
+			ret = true;
+			break;
+		}
+	}
+	return ret;
+}
+
+bool isuser(char* str) { //출력해야하는 user인지 판단
+	if(special_user == 0) return true; //출력해야하는 user의 수가 0인 경우 출력
+	bool ret = false;
+	if(!strcmp(users, str)) {
+		ret = true;
+	}
+
+	return ret;
+}
+
+void print_1() { //나열모드가 아닐 때 결과를 출력하는 함수
 	//터미널 크기 구하기
 	struct winsize win; 
 	if(ioctl(0, TIOCGWINSZ, (char*)&win) < 0) {
 		fprintf(stderr, "ioctl error\n");
 		exit(1);
 	}
-	
+
 	char tm[8]; //TIME 문자열 저장함수
 	for(int i = start_row; i < tasks; i++) {
-		if(option_i && procs[i].CPU < 0.05) continue;
+		if(option_i && procs[i].CPU < 0.1) continue;
 		memset(tm, 0, sizeof(tm));//TIME을 문자열로 나타내기
 		int min = procs[i].TIME / 6000;
 		int sec = (procs[i].TIME - (min *6000)) / 100;
 		int rest = (procs[i].TIME - (min *6000) - (sec * 100));
 		sprintf(tm, "%d:%02d.%02d", min, sec, rest);
-		snprintf(result[print_row++], win.ws_col, "%7ld %-8s %3s %3d %7lld %6lld %6lld %c %4.1lf %4.1lf   %7s %s", procs[i].PID, procs[i].USER, procs[i].PR, procs[i].NI, procs[i].VIRT, procs[i].RES,
-				procs[i].SHR, procs[i].S, procs[i].CPU, procs[i].MEM, tm, procs[i].COMMAND);
+		char username[MAX];
+		memcpy(username, procs[i].USER, MAX);
+		if(username[7] != '\0') {
+			username[7] = '+'; //이름이 너무 긴 경우 뒤에 '+'로 표시
+			memset(username + 8, '\0', MAX - 8);
+		}
+		if(ispid(procs[i].PID) && isuser(procs[i].USER)) {//출력하고자 하는 pid만 출력
+			snprintf(result[print_row++], win.ws_col, "%7ld %-8s %3s %3d %7lld %6lld %6lld %c %4.1lf %4.1lf   %7s %s", 
+					procs[i].PID, username, procs[i].PR, procs[i].NI, procs[i].VIRT, procs[i].RES, 
+					procs[i].SHR, procs[i].S, procs[i].CPU, procs[i].MEM, tm, procs[i].COMMAND);
+		}
 	}
-	
+
 	//내용 출력
 	if(!uptime_line) {
 		for(int i = 0; i < 6; i++)
 			mvprintw(i, 0, "%s", result[i]);
-		for(int i = 6; i <= win.ws_row; i++)
+		attron(A_REVERSE);
+		mvprintw(6, 0, "%s", result[6] + begin[start_col]);
+		attroff(A_REVERSE);
+		for(int i = 7; i <= win.ws_row; i++)
 			mvprintw(i, 0, "%s", result[i] + begin[start_col]);
 	}
 	else {
 		for(int i = 0; i < 5; i++)
 			mvprintw(i, 0, "%s", result[i]);
-		for(int i = 5; i <= win.ws_row; i++)
+		attron(A_REVERSE);
+		mvprintw(5, 0, "%s", result[5] + begin[start_col]);
+		attroff(A_REVERSE);
+		for(int i = 6; i <= win.ws_row; i++)
 			mvprintw(i, 0, "%s", result[i] + begin[start_col]);
 	}
 }
 
-int get_input() { //입력을 받는 함수
-	int ret;
-	//struct termios term;
-	//tcgetattr(STDIN_FILENO, &term);
-	//term.c_lflag &= ~ICANON; //line단위 입력 끄기
-	//term.c_lflag &= ~ECHO; //입력이 터미널에 보이지 않게 하기
-	//term.c_cc[VMIN] = 1; //최소 입력 버퍼 크기
-	//term.c_cc[VTIME] = 0; //버퍼 비우는 시간
-	//tcsetattr(STDIN_FILENO, TCSAFLUSH, &term); 
-	ret = getchar();
 
-	return ret;
+void print_2() { //나열 모드 결과를 출력하는 함수
+	printf("\n");
+	//터미널 크기 구하기
+	struct winsize win; 
+	if(ioctl(0, TIOCGWINSZ, (char*)&win) < 0) {
+		fprintf(stderr, "ioctl error\n");
+		exit(1);
+	}
+
+	
+	char tm[8]; //TIME 문자열 저장함수
+	for(int i = start_row; i < tasks; i++) {
+		if(option_i && procs[i].CPU < 0.1) continue;
+		memset(tm, 0, sizeof(tm));//TIME을 문자열로 나타내기
+		int min = procs[i].TIME / 6000;
+		int sec = (procs[i].TIME - (min *6000)) / 100;
+		int rest = (procs[i].TIME - (min *6000) - (sec * 100));
+		sprintf(tm, "%d:%02d.%02d", min, sec, rest);
+		char username[MAX];
+		memcpy(username, procs[i].USER, MAX);
+		if(username[7] != '\0') {
+			username[7] = '+'; //이름이 너무 긴 경우 뒤에 '+'로 표시
+			memset(username + 8, '\0', MAX - 8);
+		}
+		if(ispid(procs[i].PID) && isuser(procs[i].USER)) {//출력하고자 하는 pid만 출력
+			snprintf(result[print_row++], win.ws_col, "%7ld %-8s %3s %3d %7lld %6lld %6lld %c %4.1lf %4.1lf   %7s %s", 
+					procs[i].PID, username, procs[i].PR, procs[i].NI, procs[i].VIRT, procs[i].RES, 
+					procs[i].SHR, procs[i].S, procs[i].CPU, procs[i].MEM, tm, procs[i].COMMAND);
+		}
+	}
+	
+	//내용 출력
+	for(int i = 0; i < print_row; i++)
+		printf("%s\n", result[i]);
 }
 
-void start_status() {
+void operation_d() { //d옵션을 수행하는 함수
+	alarm(0); //alarm 취소
+	char str[MAX];
+	memset(str, 0, MAX);
+	int idx = 0;
+	int c;
+	int chk = 0;
+	if(!uptime_line) { //uptime_line이 있는 경우
+		mvaddstr(5, 0, "Change delay from 3.0 to "); 
+	}
+	else { //uptime_line이 없는 경우
+		mvaddstr(4, 0, "Change delay from 3.0 to ");
+	}
+	while((c = getch()) != '\n') {
+		if(c == 8) { //backspace인 경우
+			idx--;
+			delch();
+			continue;
+		}
+		if(c == 27) { //esc를 누른 경우
+			chk = 1;
+			break;
+		}
+		str[idx] = c;
+		idx++;
+	}
+	if(!chk) //esc를 누른 경우 취소
+		delay = atoi(str);
+	alarm(3);
+	raise(SIGALRM);
+}
+
+void operation_u() {
+	alarm(0); //alarm 취소
+	char str[MAX];
+	memset(str, 0, MAX);
+	int idx = 0;
+	char c;
+	int chk = 0;
+	if(!uptime_line) { //uptime_line이 있는 경우
+		mvaddstr(5, 0, "Which user (blank for all) "); 
+	}
+	else { //uptime_line이 없는 경우
+		mvaddstr(4, 0, "Which user (blank for all) ");
+	}
+	//c = getch();
+	while((c = getch()) != '\n') {
+		if(c == 8) { //backspace인 경우
+			idx--;
+			delch();
+			continue;
+		}
+		if(c == 27) { //esc를 누른 경우
+			chk = 1;
+			break;
+		}
+		str[idx++] = c;
+	}
+	if(!chk) { //esc를 누르지 않았을 때
+		strncpy(users, str, MAX); //출력하고자 하는 user에 복사
+		if(idx == 0) //아무것도 입력을 받지 않은 경우
+			special_user = 0;
+		else
+			special_user = 1;
+	}
+	alarm(3);
+	raise(SIGALRM);
+
+}
+
+
+void operation_k() { //k옵션을 수행하는 함수
+	alarm(0); //alarm 취소
+	char str[MAX];
+	char sig[10];
+	char tmp[MAX];
+	char blank[MAX];
+	char trash[MAX];
+	memset(blank, ' ', 70);
+	memset(str, 0, MAX);
+	memset(sig, 0, 10);
+	int idx = 0;
+	int c;
+	int chk = 0;
+	memset(tmp, 0, MAX);
+	sprintf(tmp, "PID to signal/kill [default pid = %ld] ", procs[0].PID);
+	if(!uptime_line) { //uptime_line이 있는 경우
+		mvaddstr(5, 0, tmp); //default는 가장 위에 있는 Process
+	}
+	else { //uptime_line이 없는 경우
+		mvaddstr(4, 0, tmp);
+	}
+	while((c = getch()) != '\n') {
+		if(c == 8) { //backspace인 경우
+			idx--;
+			delch();
+			continue;
+		}
+		if(c == 27) { //esc를 누른 경우
+			chk = 1;
+			break;
+		}
+		str[idx++] = c;
+	}
+	if(!chk) {//esc를 누르지 않은 경우
+		if(idx == 0) //아무것도 입력을 받지 않은 경우
+			kill_pid = procs[0].PID;
+		else
+			kill_pid = atoi(str);
+
+		memset(tmp, 0, MAX);
+		sprintf(tmp, "Send pid %d signal [15/sigterm] ", kill_pid);
+		if(!uptime_line) { //uptime_line이 있는 경우
+			mvaddstr(5, 0, blank);
+			mvaddstr(5, 0, tmp);
+		}
+		else { //uptime_line이 없는 경우
+			mvaddstr(4, 0, blank);
+			mvaddstr(4, 0, tmp);
+		}
+		idx = 0;
+		tcflush(0, TCIFLUSH);
+		getch();
+		while((c = getch()) != '\n') {
+			if(c == 8) { //backspace인 경우
+				idx--;
+				delch();
+				continue;
+			}
+			if(c == 27) { //esc를 누른 경우
+				chk = 1;
+				break;
+			}
+			sig[idx++] = c;
+		}
+		if(!chk) { //esc를 누르지 않은 경우
+			int signal = atoi(sig);
+			//printf("%s\n", sig);
+			//exit(0);
+			if(signal == 9) {
+				kill(kill_pid, SIGKILL);
+			}
+		}
+	}
+	alarm(3);
+	raise(SIGALRM);
+}
+
+
+
+void start_status() { //ncurses 초기 설정
 	noecho(); //echo 제거
 	curs_set(0); //커서 안보이게 함
 	initscr(); //출력 윈도우 초기화
 	halfdelay(10); //0.1초마다 갱신
+	keypad(stdscr, true);
 }
 
-void return_status() {
-	endwin();
-	echo();
-}
 
-int main() {
+int main(int argc, char **argv) {
 
 	for(int i = 12; i < MAX; i++) begin[i] = begin[i-1] + 8;
-	start_status();
+
 	//초기 옵션 설정
+	signal(SIGALRM, handler); //alarm 시그널에 동작할 handler함수를 설정
 	option = 'P';
 	uptime_line = 0;
 	delay = 3;
 	option_c = 0;
 	option_i = 0;
+	print_max = -1;
+	special_pid = 0;
+	option_b = 0;
+	special_user = 0;
+	kill_pid = 0;
 
-	signal(SIGALRM, handler);
+	//실행 전 옵션 parsing
+	for(int i = 1; i < argc; i++) {
+		if(!strcmp(argv[i], "-n")) { //실행 횟수 제한옵션
+			print_max = atoi(argv[i+1]);
+			if(print_max == 0) { //올바르지 않은 숫자를 입력한 경우 에러처리 후 종료
+				fprintf(stderr, "top: bad iterations argument '0'\n");
+				endwin();
+				exit(0);
+			}
+		}
+		if(!strcmp(argv[i], "-p")) { //특정 PID만 출력
+			int pid = atoi(argv[i+1]);
+			pids[special_pid] = pid;
+			special_pid++; //출력 pid 개수 증가
+		}
+		if(!strcmp(argv[i], "-b")) { //모든 process들을 나열하기만 하는 경우
+			option_b = 1;
+		}
+	}
+
 	get_data();
 	sort_by_cpu();
-	print();
-	refresh();
-
+	if(!option_b) {
+		start_status();
+		print_1();
+		refresh();
+	}
+	else {
+		print_2();
+	}
+	print_cnt = 1;
 	//3초마다 갱신
-	
 	while(1) {
+		if(print_max != -1 && print_max == print_cnt) break;
 		alarm(delay);
-		int input = get_input();
+		int input = getchar();
 		int sum = input;
 		if(input == 'q') { //종료
-			exit(0);
+			break;
 		}
 		else if(input == 'P') { //CPU순 정렬
 			option = 'P';
@@ -585,8 +848,10 @@ int main() {
 			raise(SIGALRM);
 		}
 		else if(input == 'l') { //uptime_line숨김/표시
-			if(uptime_line) uptime_line = 0;
-			else uptime_line = 1;
+			if(uptime_line)
+				uptime_line = 0;
+			else
+				uptime_line = 1;
 			raise(SIGALRM);
 		}
 		else if(input == ' ') { //refresh
@@ -602,10 +867,18 @@ int main() {
 			else option_i = 1;
 			raise(SIGALRM);
 		}
-		
+		else if(input == 'd') { //delay를 설정
+			operation_d();
+		}
+		else if(input == 'u' || input == 'U') { //특정 user 설정
+			operation_u();
+		}
+		else if(input == 'k') { //kill하려는 pid 출력
+			operation_k();
+		}
 		else { //방향키를 입력하는 경우
 			for(int i = 0; i < 2; i++) {
-				input = get_input();
+				input = getchar();
 				sum += input;
 			}
 			if(sum == 183) {//위 방향키인 경우
@@ -614,9 +887,9 @@ int main() {
 				raise(SIGALRM);
 			}
 			else if(sum == 184) { //아래 방향키인 경우
-				 start_row++;
-				 if(start_row == tasks) start_row = tasks-1;
-				 raise(SIGALRM);
+				start_row++;
+				if(start_row == tasks) start_row = tasks-1;
+				raise(SIGALRM);
 			}
 			else if(sum == 186) { //왼쪽 방향키인 경우
 				start_col--;
@@ -630,7 +903,6 @@ int main() {
 		}
 	}
 
-	return_status();
-
+	endwin();
 	return 0;
 }
